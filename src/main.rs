ae53210fc53c347750e031218f8d20f1b03fbf99
Karol Sudol <karol@sudoltrust.com>
2024-12-11 16:26:46 +0000

    fix: exclude spam

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ struct OutputRecord {
 
 async fn fetch_balances(client: &reqwest::Client, address: &str) -> Result<ApiResponse, Box<dyn Error>> {
     let url = format!(
-        "https://api.dune.com/api/echo/v1/balances/evm/{}",
+        "https://api.dune.com/api/echo/v1/balances/evm/{}?exclude_spam_tokens=exclude_spam_tokens",
         address
     );
     
